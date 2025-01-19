3020521240e639639539eb41c35e2881b8eac696
Thanu793 <thanurai2000@gmail.com>
2025-01-19 07:02:09 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+document.getElementById('menu-icon').addEventListener('click, function() {const menu =
+                                                      document.getElementById('menu');
+if (menu.style.display === 'block') {
+  menu.style.display = 'none';
+}else{
+  menu.style.display = 'block';
+}
+});
