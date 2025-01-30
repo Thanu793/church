c76ce18383cec2267d680cdee25caa0cc0c611de
Thanurai <thanurai2000@gmail.com>
2025-01-30 19:28:34 +0400

    updated css for responsive webpage

--- script.js
+++ script.js
@@ -41,13 +41,4 @@ function showVerse() {
     document.getElementById('verse').innerText = verses[randomIndex];
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const buttons = document.querySelectorAll("a");
 
-    buttons.forEach(button => {
-        button.addEventListener("click", function (event) {
-            event.preventDefault();
-            alert("This is a demo site. Functionality is coming soon!");
-        });
-    });
-});
