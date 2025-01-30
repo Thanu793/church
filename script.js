ddfb8e8df5a4a016e4d416c04377871333d6cfa1
Thanurai <thanurai2000@gmail.com>
2025-01-30 17:29:46 +0400

    updated homepage

--- script.js
+++ script.js
@@ -40,3 +40,14 @@ function showVerse() {
     const randomIndex = Math.floor(Math.random() * verses.length);
     document.getElementById('verse').innerText = verses[randomIndex];
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const buttons = document.querySelectorAll("a");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", function (event) {
+            event.preventDefault();
+            alert("This is a demo site. Functionality is coming soon!");
+        });
+    });
+});
