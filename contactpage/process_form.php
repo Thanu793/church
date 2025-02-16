4f71e6c83ad30edfa76d1adbd988a905a075bce5
Thanurai <thanurai2000@gmail.com>
2025-02-16 22:47:12 +0400

    updated file

--- contactpage/process_form.php
+++ contactpage/process_form.php
@@ -1,14 +1,17 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collect form data
     $name = htmlspecialchars($_POST['name']);
     $email = htmlspecialchars($_POST['email']);
     $message = htmlspecialchars($_POST['message']);
 
+    // Email details
     $to = "gorkhachristianchurch1998@gmail.com"; // Replace with your email address
-    $subject = "New Contact Us Message";
+    $subject = "New Contact Form Submission";
     $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
     $headers = "From: $email";
 
+    // Send email
     if (mail($to, $subject, $body, $headers)) {
         echo "Message sent successfully!";
     } else {
